179f68ab87b7476b08d6e24753f25299ec50b80f
Juliana Kang <julianajlk@gmail.com>
2018-07-13 04:06:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
