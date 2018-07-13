5d3ccd6aae65546092e5572eed143f3b2b0990cf
Juliana Kang <julianajlk@gmail.com>
2018-07-13 04:14:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_supplies.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+      
 end
 
 def all_holidays_with_bbq(holiday_hash)
