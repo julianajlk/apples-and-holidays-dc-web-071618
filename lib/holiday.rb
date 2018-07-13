dfe3a52b9b38460329a6fe3cac243e92e4d9c3b2
Juliana Kang <julianajlk@gmail.com>
2018-07-13 04:19:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday = last_holiday.join(" ")
     supply = supply.join(", ")
     puts "#{holiday}: #{supply}"
-
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
