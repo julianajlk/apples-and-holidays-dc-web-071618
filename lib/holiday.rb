3ca1b1af8be9570f667b6f167bb3832cac8973ef
Juliana Kang <julianajlk@gmail.com>
2018-07-13 04:25:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
       array = holiday.to_s.split("_")
       last_holiday = []
       array.each do |w|
-        last_holiday<<w.capitalize!
+        last_holiday<<w.capitalize
       end
     holiday = last_holiday.join(" ")
     supply = supplies.join(", ")
